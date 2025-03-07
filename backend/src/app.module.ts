3141904c3bff5de16f69beca73819b52807a89ab
Amnat Chattamanokul <amnat@oxygenai.com>
2025-03-07 16:56:17 +0700

    feat: app module

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersController } from './users/users.controller';
+import { CommentsController } from './comments/comments.controller';
+import { PostsController } from './posts/posts.controller';
+import { CommentsService } from './comments/comments.service';
+import { PostsService } from './posts/posts.service';
+import { CommentsRepository } from './comments/comments.repostiory';
+import { PostsRepository } from './posts/posts.repostiory';
+import { UsersModule } from './users/user.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [UsersModule, PrismaModule, AuthModule],
+  controllers: [
+    AppController,
+    UsersController,
+    CommentsController,
+    PostsController,
+  ],
+  providers: [
+    AppService,
+    CommentsService,
+    PostsService,
+    CommentsRepository,
+    PostsRepository,
+  ],
 })
 export class AppModule {}
