6b07317d2520889c575a4e63e0a81cfcb4ec9968
Amnat Chattamanokul <amnat@oxygenai.com>
2025-03-07 16:56:07 +0700

    feat: auth passport jwt

---
+++ backend/src/auth/auth.module.ts
@@ -0,0 +1,20 @@
+// auth.module.ts
+import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { JwtStrategy } from './jwt.strategy';
+import { UsersModule } from 'src/users/user.module';
+
+@Module({
+  imports: [
+    UsersModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
+  providers: [JwtStrategy],
+  exports: [JwtModule],
+})
+export class AuthModule {}
---
+++ backend/src/auth/jwt.strategy.ts
@@ -0,0 +1,23 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { UsersService } from 'src/users/users.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly usersService: UsersService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.JWT_SECRET,
+    });
+  }
+
+  async validate(payload: { sub: number; username: string }) {
+    const user = await this.usersService.findOneByUsername(payload.username);
+    if (!user) {
+      throw new UnauthorizedException('User no longer exists');
+    }
+
+    return user;
+  }
+}
